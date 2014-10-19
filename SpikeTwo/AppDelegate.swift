d66a8c08a14f373eb4bd15552550ec185e2b858a
erukiti <erukiti@gmail.com>
2014-10-20 01:35:45 +0900

    SplitView で分割表示できるところまで

--- SpikeTwo/AppDelegate.swift
+++ SpikeTwo/AppDelegate.swift
@@ -1,27 +1,58 @@
+// Copyright (c) 2014, erukiti at gmail dot com
+// All rights reserved.
 //
-//  AppDelegate.swift
-//  SpikeTwo
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
-//  Created by 佐々木俊介 on 2014/10/20.
-//  Copyright (c) 2014年 ponix.org. All rights reserved.
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
 //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.import Cocoa
 
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var window: NSWindow!
-
-
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        //        var blurryView = NSVisualEffectView(frame: NSRect(x: 0, y: 0, width: window.frame.width, height: window.frame.height))
+        //        blurryView.blendingMode = NSVisualEffectBlendingMode.BehindWindow
+        //        blurryView.material = NSVisualEffectMaterial.Dark
+        //        blurryView.state = NSVisualEffectState.Active
+        
+        var splitView = NSSplitView(frame: window.contentView!.bounds)
+        var filerView1 = NSTableView()
+        var filerView2 = NSTableView()
+        
+        
+        
+        
+        splitView.dividerStyle = NSSplitViewDividerStyle.Thin
+        splitView.vertical = true
+        splitView.addSubview(filerView1)
+        splitView.addSubview(filerView2)
+        splitView.adjustSubviews()
+        
+        window.contentView!.addSubview(splitView)
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
