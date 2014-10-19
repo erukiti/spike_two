3b907c80f65e2f107c50ba65a67641025dd1d4b5
erukiti <erukiti@gmail.com>
2014-10-20 01:22:56 +0900

    Initial Commit

---
+++ SpikeTwo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpikeTwo
+//
+//  Created by 佐々木俊介 on 2014/10/20.
+//  Copyright (c) 2014年 ponix.org. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
